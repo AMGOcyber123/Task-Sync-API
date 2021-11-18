7c8667b650562e28dc2078b872288c63f2492234
AMGOcyber123 <aayumodi123@gmail.com>
2021-11-19 00:49:11 +0530

    added env file

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
-
-mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
+const link = 'mongodb://127.0.0.1:27017/task-manager-api' || process.env.DB_URI
+mongoose.connect(link, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useUnifiedTopology: true,
