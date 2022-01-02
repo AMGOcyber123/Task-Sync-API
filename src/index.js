7b14fb3a7697600b5471b9454c24bbbe29a7cbb4
AMGOcyber123 <aayumodi123@gmail.com>
2022-01-02 13:27:12 +0530

    dotenv added and configured

--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
 const express = require('express')
-const bcrypt = require('bcryptjs')
-const jwt = require('jsonwebtoken')
 const taskRoute = require('./routes/taskRoute')
 const userRoute = require('./routes/userRoute')
-const Task = require('./model/task')
-const User = require('./model/user')
 require('./db/mongoose')
 const app = express()
-const port = process.env.PORT || 3000
+const dotenv = require('dotenv')
+dotenv.config();
+
+const port = process.env.PORT || 8080
 
 // app.use((req,res,next) =>{                          //can help in validating 
 //     // console.log(req.method, req.path)
@@ -35,41 +34,3 @@ app.use(taskRoute)
 app.listen(port, () => {
     console.log('Server is up on port ' + port)
 })
-
-const pet = {
-    name : 'hal'
-}
-
-// pet.toJson = function()
-// {
-//     console.log(this)
-//     return this
-// }
-// console.log(JSON.stringify(pet))
-
-
-// const task = async() =>{
-//     // const task = await Task.findById('61863b11558b4a362874575a')
-//     // await task.populate('creator').execPopulate()
-//     // console.log(task.creator)
-//     const user = await User.findById('618646cbcc068a45ac44d685')
-//     await user.populate('tasks').execPopulate()
-//     console.log(user.tasks)
-// }
-
-// task()
-
-
-
-/*
- "email" : "ajay23@gmail.com",
- "password" : "1werty123456",
- "email" : "boati23@gmail.com",
- "password" : "werty123456",
- "email" : "kacci@gmail.com",
- "password" : "123456789aa",
- "email" : "carlp@yahoo.com",
- "password" : "123456789aa",
- "email" : "kumar1992@yahoo.com",
- "password" : "1234789aa",
-*/
